3460c2bf5bea13eee9f7e41085ad584c4ed184bd
DoHyang Kim <hanoul1124@gmail.com>
2019-04-18 22:55:19 +0900

    Travis #5

--- app/members/tests.py
+++ app/members/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+User = get_user_model()
 # Create your tests here.
+
+class RegisterUser(TestCase):
+    @classmethod
+    def make_dummy_user(self):
+        User.objects.create_user(
+            username='Tester01',
+            email='test01@test.com',
+            password='1111',
+            name='tester'
+        )
+
+    def print_tester_user_email(self):
+        test_user = User.objects.get(username='Tester01')
+        print(test_user.email)
